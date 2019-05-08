8b7c7704085d1dfeb2af90676cbbe2f91f281864
Tomek <tomasz.klepacz95@gmail.com>
2019-05-08 13:02:43 +0200

    add spring profiles configuration

--- src/main/java/com/tomek/camelspringboot/route/CamelRoute.java
+++ src/main/java/com/tomek/camelspringboot/route/CamelRoute.java
@@ -1,15 +1,25 @@
 package com.tomek.camelspringboot.route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CamelRoute extends RouteBuilder {
+
+    private final Environment environment;
+
+    @Autowired
+    public CamelRoute(Environment environment) {
+        this.environment = environment;
+    }
+
     @Override
     public void configure() {
-        from("timer:hello?period=10s")
-                .log("Timer invoked and the body ${body}")
-                .pollEnrich("file:data/input?delete=true&readLock=none")
-                .to("file:data/output");
+        from("{{startRoute}}")
+                .log("Timer invoked and the body " + environment.getProperty("message"))
+                .pollEnrich("{{fromRoute}}")
+                .to("{{toRoute}}");
     }
 }
